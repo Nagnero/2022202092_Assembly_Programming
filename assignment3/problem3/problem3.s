c5371f5b6a3370292c8fa9f9b49f3d05b76597a2
sunwoo <davidyeon123@naver.com>
2023-10-15 00:14:00 +0900

    assignment3: complete problem1 and problem3

--- assignment3/problem3/problem3.s
+++ assignment3/problem3/problem3.s
@@ -6,21 +6,50 @@ start
 	MOV	r1, #0
 	MOV r2, #1
 	
-	BL	Example1
-	BL	Example2
-	BL	Example3
+	B	Example1
+	
 	
 Example1	; Loop
 	ADD r1, r2
 	ADD r2, r2, #1
 	CMP r2, #11
 	BNE	Example1
+	STR r1, [r0], #4
 
 	
 Example2	; n(n+1)/2
+	MOV r1, #10
+	ADD r2, r1, 1
+	MUL r3, r1, r2
+	LSR r1, r3, 1
+	STR r1, [r0], #4
 
 
 Example3	; Unlooping
+	MOV r1, #0
+	MOV r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	ADD r1, r2
+	ADD r2, #1
+	STR r1, [r0]
+	
 	
 	
 TEMPADDR1 & &40000
